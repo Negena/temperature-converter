07c2315fa28b7dab804abbc66054e9e134a701da
Nigina Muhammadieva <niginamuhammadieva@MacBook-Pro-Nigina.local>
2022-08-19 18:01:51 +0500

    push

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 $(function(){
+  //eventlistener
   $("#input").on("input", convert);
   $("#input").on("change", convert);
   $(".convertWhat").on("change", convert)
 
+  $("#input").attr("placeholder", "celsius")
+
   function convert(){
     let selection = $(".convertWhat :selected").text().toLowerCase();
     let input = $('#input').val()
+
+    //if input is empty string
     $("#input").attr("placeholder", selection);
-    if ($('#input').val() === " " ){
-      $("#num").val("no input")
-    }
-    if (selection  === 'celsius') {
+
+    if ($('#input').val() == Number || $("#input").val() == "")  {
+     $("#num").val(" ")
+    }else {
+
+     //if option selected is celsius
+     if (selection  === 'celsius') {
+      
       const calcFar = parseFloat(( input * 9/5 ) + 32)
+      $("#input").attr("placeholder", "celsius")
       $("#num").val(calcFar+"℉")
     }
-    if (selection === "fahrenheit") {
+
+     //if option selected is fahrenheit
+     if (selection === "fahrenheit") {
       const calcCel = parseFloat((input - 32) / 1.8)
+      $("#input").attr("placeholder", "fahrenheit")
       $("#num").val(calcCel+'℃')
     }
+        
+  }
+
   }
 })
